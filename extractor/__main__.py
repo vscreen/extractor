72d7af67b5a9ffecf6f02723e5f6b5779912ec7e
Lukas <lherman.cs@gmail.com>
2019-04-02 10:53:09 -0400

    Initial commit

---
+++ extractor/__main__.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+import sys
+import youtube_dl
+
+ydl_opts = {
+    "format": "best",
+    "dump-json": True
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    extract = ydl.extract_info
+    readline = sys.stdin.readline
+    while 1:
+        try:
+            url = readline()
+        except KeyboardInterrupt:
+            break
+
+        if not url:
+            break
+
+        print(extract(url, download=False))
